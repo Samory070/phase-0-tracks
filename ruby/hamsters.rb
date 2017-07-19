595957e5c0078f3311d4a163929c55d1093c310c
Samory Harris <samory070@gmail.com>
2017-07-19 16:18:53 -0400

    Initial commit, store question answers in variables

---
+++ ruby/hamsters.rb
@@ -0,0 +1,17 @@
+est_age = nil
+
+puts "What's the hamster's name?"
+name = gets.chomp
+
+puts "What's your sleep sensitivity from 1-10?"
+volume = gets.chomp.to_i
+
+puts "What's the color of the hamster's fur?"
+fur_color = gets.chomp
+
+puts "Would you recommend this hamster for adoption (y/n)?"
+     input = gets.chomp
+
+puts "How old would you say this hamster is?"
+est_age = gets.chomp.to_i
+
