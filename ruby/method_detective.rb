38bcd8c3272f394b0952f070b8fc4d9d301ba814
Samory Harris <samory070@gmail.com>
2017-07-25 11:16:30 -0400

    Initial commit, add methods to str

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,40 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+puts "iNvEsTiGaTiOn".swapcase
+#=> “InVeStIgAtIoN”
 
-# "zom".<???>
-# => “zoom”
+puts "zom".insert(1, 'o')
+#=> “zoom”
 
-# "enhance".<???>
-# => "    enhance    "
+puts "enhance".center(20)
+#=> "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+puts "Stop! You’re under arrest!".upcase
+#=> "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+puts "the usual".concat(" suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
-# => "the usual suspects"
+puts " suspects".insert(0, "the usual" )
+#=> "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+puts "The case of the disappearing last letter".chop
+#=> "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
-# => "he mystery of the missing first letter"
+puts "The mystery of the missing first letter".reverse.chop.reverse  
+#puts "The mystery of the missing first letter"[ 1 .. -1 ]
+#puts "The mystery of the missing first letter".sub('T', '') 
+#=> "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
-# => "Elementary, my dear Watson!"
+puts "Elementary,    my   dear        Watson!".squeeze
+#=> "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+puts "z".codepoints()
+#=> 122 
+#(What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
-# => 4
+puts "How many times does the letter 'a' appear in this string?".count('a')
+#=> 4
+
+
