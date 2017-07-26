adb7a8043068385c6cf6ea2a7b759f68377d335d
Samory Harris <samory070@gmail.com>
2017-07-26 11:01:10 -0400

    Add description notes

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,39 +4,50 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-puts "iNvEsTiGaTiOn".swapcase
+# swaps lowercase to uppercase and vice vera
+puts "iNvEsTiGaTiOn".swapcase 
 #=> “InVeStIgAtIoN”
 
-puts "zom".insert(1, 'o')
+#1st arg. represent the index and the 2nd arg. is the inserted value
+puts "zom".insert(1,'o')
 #=> “zoom”
 
+#centers str and the arg. is the amt. of whitespace on either side
 puts "enhance".center(20)
 #=> "    enhance    "
 
+#converts entire str to uppercase
 puts "Stop! You’re under arrest!".upcase
 #=> "STOP! YOU’RE UNDER ARREST!"
 
+#appends str to another str
 puts "the usual".concat(" suspects")
 #=> "the usual suspects"
 
-puts " suspects".insert(0, "the usual" )
+#appends str to begining of the str
+puts " suspects".prepend("the usual")
 #=> "the usual suspects"
 
+#removes last character in the str.
 puts "The case of the disappearing last letter".chop
 #=> "The case of the disappearing last lette"
 
+#reverses entire str, removes last character, them reverses it again
 puts "The mystery of the missing first letter".reverse.chop.reverse  
-#puts "The mystery of the missing first letter"[ 1 .. -1 ]
 #puts "The mystery of the missing first letter".sub('T', '') 
 #=> "he mystery of the missing first letter"
 
-puts "Elementary,    my   dear        Watson!".squeeze
+#Takes away  the whitespace between content
+puts "Elementary,    my   dear        Watson!".squeeze(' ') 
+#.gsub(/\s+/, "") 
 #=> "Elementary, my dear Watson!"
 
+#codepoints returns unicode codepoints.
 puts "z".codepoints()
 #=> 122 
 #(What is the significance of the number 122 in relation to the character z?)
 
+#counts how many occurences of the given arg.
 puts "How many times does the letter 'a' appear in this string?".count('a')
 #=> 4
 
