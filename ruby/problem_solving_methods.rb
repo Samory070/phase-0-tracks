d7e8202e1c373d1a4bbc0a70016718ea4a0dbdbb
Samory Harris <samory070@gmail.com>
2017-07-30 15:01:17 -0400

    Add release 2

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -16,3 +16,24 @@ def search_array(array, num)
 end
 
 p search_array(array, 27)
+
+
+def fib(num_of_numbers)
+  fib_array = [0,1]
+  num1 = 0
+  num2 = 1
+  while fib_array.length < num_of_numbers
+    num3 = num1 + num2
+    fib_array.push(num3)
+    
+    num1 = num2
+    num2 = num3
+  end
+  return fib_array
+end
+p fib(100)
+if fib(100).last == 218922995834555169026
+  puts 'Your code works'
+else
+  puts 'Try again'
+end
