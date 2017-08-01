29324d21119bd9246fcda51e35cf2c2e6c49fec2
Samory Harris <samory070@gmail.com>
2017-08-01 11:49:13 -0400

    Add release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,33 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
+  
+  def speak(int)
+  	puts 'Woof!' * int
+  end
+
+  def roll_over
+  	puts '*Rolls over*'
+  end
+
+  def dog_years(a)
+  	b = a * 7
+  	puts "You are #{b} in dog years"
+  end	
+  
+  def jump
+  	puts 'Up boy!'
+  end
 
 end
+
+p 'Created new Puppy class' 
+fido = Puppy.new 
+sassy = Puppy.new 
+lou = Puppy.new
+sassy.fetch('ball')
+fido.speak(3)
+sassy.roll_over
+fido.dog_years(28)
+lou.jump
 
