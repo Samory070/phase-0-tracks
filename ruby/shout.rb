89a98a9dd2589b39c5b58ad8a2f19333d053457a
Samory Harris <samory070@gmail.com>
2017-08-07 09:39:20 -0400

    Add release 2+3

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,42 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#   	words + "!?!" + ":)"
+#   end
+# end
+#p Shout::yelling_happily("wah gwan") # . before method works as well
+
 module Shout
-  def self.yell_angrily(words)
+  
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
+  def yell_happily(words)
   	words + "!?!" + ":)"
   end
 end
 
-p Shout::yelling_happily("wah gwan")
+class Kid
+   include Shout
+ end
+  		  
+ class Librarian 
+   include Shout
+  end		  
+
+dude = Kid.new  		  
+
+ 
+ p dude.yell_angrily("homework")
+ p dude.yell_happily("party on the beach?")
+ 
+ educator = Librarian.new
+ p educator.yell_happily("nice a quite")
+ p educator.yell_angrily("SSSHHHHHH")
+ 
+
+
