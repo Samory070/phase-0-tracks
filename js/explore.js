8ecc37f6bd8803a1e355ddbf9696849c940b1a36
Samory Harris <samory070@gmail.com>
2017-08-11 11:32:44 -0400

    Add psuedocode

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+//function takes as string
+//break each character into a array with .split("")
+//reverse order of array(first to last) with .reverse()
+//put sting back together with .join("")
+//print string to console
+
