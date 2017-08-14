d1370bb5ee291bbfbe3cc704c95be8ddb85968ad
Samory Harris <samory070@gmail.com>
2017-08-14 07:08:12 -0400

    Add release 0

--- js/algos.js
+++ js/algos.js
@@ -3,4 +3,23 @@
 //if length of element is greater than longest_word
  // store length of element in phrase_length
 
+var phrase_array = ["long phrase","longest phrase","longer phrase"];
+
+
+function longest_word(str) {
+
+var longestWord = 0;
+var phrase_length = 0;
+  for(var i = 0; i < str.length; i++){
+    if(str[i].length > longestWord){ 
+	longestWord = str[i].length; 
+	phrase_length = i;
+     }
+  }
+  return longest_word[phrase_length];
+}
+
+console.log(longest_word("long phrase", "longest phrase","longer phrase"));
+
+
 
