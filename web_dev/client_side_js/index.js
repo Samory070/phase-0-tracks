1229d4d50e6b350263b1ed3017f9674a1ed7d13e
Samory Harris <samory070@gmail.com>
2017-08-23 11:31:26 -0400

    Add release 1

---
+++ web_dev/client_side_js/index.js
@@ -0,0 +1,15 @@
+function boldText(event) {
+    // id.innerHTML = "Ooops!";
+		event.target.style.fontWeight = "900";
+}
+
+function addBorder(event) {
+	event.target.style.border = "2px dotted purple";
+}
+
+var paragraph1 = document.getElementById("paragraph_id");
+paragraph1.addEventListener("click", boldText);
+
+var paragraph2 = document.getElementById("paragraph_id2");
+paragraph2.addEventListener("click", addBorder);
+
